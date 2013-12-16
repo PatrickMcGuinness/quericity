e950e8a94e7ec021791ff6a2d56c2fea359b49de
Salman Aslam <macbook@Macs-MacBook-Pro.local>
2013-12-16 18:02:17 +0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuizLib::Application.config.secret_token = 'b62304e3608f5f0d6ab38db574ed2ce624a3a39f73601dd3c82689090a7d6ee7cf3bef4d33c5a8a4bf63988bd1ff68368c512bbbcc301fe8ae817fe109ad2b7f'
