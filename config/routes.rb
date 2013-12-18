3b60690fda351f94658ba6c28570a8e98d622a0f
Salman Aslam <macbook@Macs-MacBook-Pro.local>
2013-12-18 19:59:59 +0500

    added tabs

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,7 @@ QuizLib::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  resources :repositories
   root :to => "home#index"
   # See how all your routes lay out with "rake routes"
 
