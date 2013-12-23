bd19d61bb15be9711e8de21a535561b1f574c7cb
Salman Aslam <macbook@Macs-MacBook-Pro.local>
2013-12-23 18:50:00 +0500

    fixe

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,6 +1,11 @@
 class Repository < ActiveRecord::Base
   attr_accessible :description, :title
   validates :title, :presence => true, :length => { minimum: 5 }
+  
   belongs_to :user
+  
+  #has_many :user_repositories
+  #has_many :collaborators , :through => user_repositories
+  
   has_many :quiz_banks
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131219133550) do
+ActiveRecord::Schema.define(:version => 20131223122228) do
 
   create_table "quiz_banks", :force => true do |t|
     t.string   "title"
@@ -33,6 +33,14 @@ ActiveRecord::Schema.define(:version => 20131219133550) do
     t.string   "title"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "user_repositories", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "repository_id"
+    t.string   "permission"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
   end
 
   create_table "users", :force => true do |t|
