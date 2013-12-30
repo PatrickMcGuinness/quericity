0409b5f959eb2e51bbbb2be667fc1fae48b3452a
salmanaslam25@gmail.com <anser.naseer@clustox.com>
2013-12-30 16:45:38 +0500

    Security Issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ApplicationController
-
+	before_filter :require_login
+	
 	def profile
     @user = User.find(params[:id])
     respond_to do |format|
       format.html
+    end
+  end
+
+
+  def require_login
+    unless current_user
+      redirect_to new_user_session_path, :notice => "Please log in to access this page."
     end
   end
 end
