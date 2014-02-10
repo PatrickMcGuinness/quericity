e0dcb60a63f62568fd8796ab74a7203304439639
abdullah <abdullah.khan.niazi@clustox.com>
2014-02-10 10:06:06 +0500

    bugs fixed

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,8 +1,6 @@
 class TopicsController < ApplicationController
   before_filter :authenticate_user!
   
-  def create
-  end
   def search
     @topics = Topic.search(params[:search])
     render json:{topics:@topics}
