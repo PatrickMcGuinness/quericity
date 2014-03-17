ea29cfde321a6f32fa6790066c68bc3c0ef2775e
abdullah <abdullah.khan.niazi@clustox.com>
2014-03-17 19:16:33 +0500

    sdfsdf

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,7 +3,7 @@ var quizlib = angular.module('quizlib', ['ngResource', 'ngRoute','ui.bootstrap']
 quizlib.config(['$routeProvider',function($routeProvider) {
     $routeProvider.
       when('/manage_quiz_banks', 
-      {templateUrl: '/assets/partials/manage_quiz_banks/index.html',
+      {templateUrl: 'http://quiz-lib.herokuapp.com/assets/partials/manage_quiz_banks/index.html',
       controller: 'ManageCtrl'}).
       when('/quiz_banks/new',
       {templateUrl: '/assets/partials/quiz_banks/new.html',controller: 'NewQuizBankCtrl'}).
