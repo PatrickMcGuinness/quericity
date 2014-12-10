7ca388c2c15db3436b31f5875d6c4cfeb493cebf
jalaluddeen <jell047@gmail.com>
2014-12-10 19:28:07 +0530

    adding dateFormat and status check as  service adding  take quiz,finishquiz, quizlist dateformat change

--- app/assets/javascripts/student.js
+++ app/assets/javascripts/student.js
@@ -2,21 +2,23 @@ var student_quizlib = angular.module('student_quizlib', ['ngResource', 'ngRoute'
 
 student_quizlib.config(['$routeProvider',function($routeProvider) {
     $routeProvider.
-      when('/quizlist', 
-      {templateUrl: 'assets/partials/students/quizlist.html',controller: 'QuizListCtrl',activetab: 'quiz'}).
+      when('/dashboard', 
+      {templateUrl: 'assets/partials/students/dashboard.html',controller: 'QuizListCtrl',activetab: 'dashboard'}).
       when('/quiz/:id/attempt', 
       {templateUrl: 'assets/partials/students/attempt.html',controller: 'QuizAttemptCtrl',activetab: 'quiz'}).
       when('/quiz/:id/answers', 
       {templateUrl: 'assets/partials/students/answers.html',controller: 'AnswersCtrl',activetab: 'quiz'}).
-      when('/dashboard', 
-      {templateUrl: 'assets/partials/students/dashboard.html',controller: 'DashBoardCtrl',activetab: 'dashboard'}).
-      when('/dashboard/quiz/:id',
-      {templateUrl: 'assets/partials/students/quiz_detail.html',controller: 'QuizDetailCtrl',activetab: 'dashboard'}).
+      when('/reports', 
+      {templateUrl: 'assets/partials/students/reports.html',controller: 'DashBoardCtrl',activetab: 'dashboard'}).
+      when('/reports/quiz/:id',
+      {templateUrl: 'assets/partials/students/quiz_detail.html',controller: 'QuizDetailCtrl',activetab: 'reports'}).
       when('/settings',
       {templateUrl: 'assets/partials/settings/settings.html',controller: 'SettingsCtrl',activetab: 'settings'}).
       when('/take_quiz',
       {templateUrl: 'assets/partials/students/take_quiz.html',controller: 'TakeQuizCtrl',activetab: 'quiz'}).
+      when('/finish_quiz',
+      {templateUrl: 'assets/partials/students/finishQuiz.html',controller: 'FinishQuizCtrl',activetab: 'quiz'}).
        when('/enrol_group',
       {templateUrl: 'assets/partials/students/enrol_group.html',controller: 'EnrolGroupCtrl',activetab: 'groups'}).
-      otherwise({redirectTo: '/quizlist'})
+      otherwise({redirectTo: '/dashboard'})
   }]);
